98f3bd79a6498f7a228d5af8de97b13bee7c02bd
douglas-front <douglasjknw007@gmail.com>
2024-11-11 23:20:51 -0300

    feat: nav, button component and hero added

--- src/app/layouts/hero/hero.component.ts
+++ src/app/layouts/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../components/button/button.component";
+import { MarqueeComponent } from "../../components/marquee/marquee.component";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, MarqueeComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
