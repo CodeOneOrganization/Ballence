dac662ba1b605772c30456451f471f05da67eff1
douglas-front <douglasjknw007@gmail.com>
2024-11-15 11:30:29 -0300

    feat: footer finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeroComponent } from "./layouts/hero/hero.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { HighFashionClothingsComponent } from "./layouts/high-fashion-clothings/high-fashion-clothings.component";
 import { NewsComponent } from "./layouts/news/news.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, NavComponent, HighFashionClothingsComponent, NewsComponent],
+  imports: [RouterOutlet, HeroComponent, NavComponent, HighFashionClothingsComponent, NewsComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
