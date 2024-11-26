4a30afb4a9ba03cc26dc64fccb15bc21580dbef2
douglas-front <douglasjknw007@gmail.com>
2024-11-26 18:47:38 -0300

    fix:removed unnecessary variables

--- src/app/layouts/about/about.component.ts
+++ src/app/layouts/about/about.component.ts
@@ -14,8 +14,6 @@ import { CursorInteractiveSlideComponent } from "../../components/cursor-interac
 
 export class AboutComponent implements AfterViewInit {
 
-  isMouseEventMen: boolean = false;
-  isMouseEventWoman: boolean = false;
   isMouseEvent: boolean = false;
   image!: string
 
@@ -23,66 +21,39 @@ export class AboutComponent implements AfterViewInit {
     this.menMouseEvent()
     this.womanMouseEvent()
 
-
-  }
-
-  menMouseEvent(): void{
-    const ref = document.getElementById('test');
     const aboutRef = document.getElementById('about');
- 
-    ref?.addEventListener('mousemove', () => {
-      this.isMouseEventMen = true;
-      this.isMouseEvent = true
-      console.log('Mouse Move:', this.isMouseEventMen);
-      this.image = '/men.jpg'
-    });
-
     aboutRef?.addEventListener('mouseenter', () => {
-      this.isMouseEventMen = false;
       this.isMouseEvent = false
-      console.log('Mouse Enter:', this.isMouseEventMen);
+
       this.image = ''
     });
 
     aboutRef?.addEventListener('mouseleave', () => {
-      this.isMouseEventMen = false;
-      this.isMouseEvent = false
-      console.log('Mouse Leave:', this.isMouseEventMen);
+      this.isMouseEvent = false;
+
       this.image = ''
+    });
+  }
+
+  menMouseEvent(): void{
+    const ref = document.getElementById('test');
+ 
+    ref?.addEventListener('mousemove', () => {
+  
+      this.isMouseEvent = true
+  
+      this.image = '/men.jpg'
     });
     
   }
 
   womanMouseEvent(): void{
-    
     const ref = document.getElementById('woman');
-    const aboutRef = document.getElementById('about');
- 
+
     ref?.addEventListener('mousemove', () => {
-      
-      this.isMouseEventWoman = true;
       this.isMouseEvent = true
-      console.log('Mouse Move:', this.isMouseEventWoman);
+   
       this.image = '/woman.jpg'
-    });
-
-    aboutRef?.addEventListener('mouseenter', () => {
-      
-      this.isMouseEventWoman = false;
-      if(!this.isMouseEventWoman){
-        ref!.style.filter = 'grayscale(0%)';
-      }
-      this.isMouseEvent = false
-      console.log('Mouse Enter:', this.isMouseEventWoman);
-      this.image = ''
-    });
-
-    aboutRef?.addEventListener('mouseleave', () => {
-      this.isMouseEventWoman = false;
-
-      this.isMouseEvent = false;
-      console.log('Mouse Leave:', this.isMouseEventWoman);
-      this.image = ''
     });
   }
 }
