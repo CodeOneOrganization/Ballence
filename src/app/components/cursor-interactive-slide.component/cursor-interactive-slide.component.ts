cc144133ef6da87499a00888db1b501320cdf48a
HuannAnd <huannvicente14@outlook.com>
2024-11-26 19:44:41 -0300

    feat: trying to implement mouseenter and mouseleave cursor animation

--- src/app/components/cursor-interactive-slide.component/cursor-interactive-slide.component.ts
+++ src/app/components/cursor-interactive-slide.component/cursor-interactive-slide.component.ts
@@ -22,8 +22,8 @@ export class CursorInteractiveSlideComponent implements OnDestroy {
       ease: 0.1
     }
   }
-  private setX = GSAP.quickSetter(".cursor-follower", "left", "px")
-  private setY = GSAP.quickSetter(".cursor-follower", "top", "px")
+  private setX!: Function
+  private setY!: Function
   private animationId: number | null = null
 
 
@@ -31,24 +31,35 @@ export class CursorInteractiveSlideComponent implements OnDestroy {
   public onMouseMove(event: MouseEvent) {
     this.mouse.x.target = event.clientX
     this.mouse.y.target = event.clientY
-    
-    console.log("onMouseMove fired")
+  }
+
+  public onMouseEnterInImage() {
+    console.log("onMouseEnterInImage fired")
+    const element = document.querySelector<HTMLDivElement>(".cursor-follower")!
+
+    element.classList.add(".visible")
+  }
+
+  public onMouseLeaveInImage() {
+    const element = document.querySelector<HTMLDivElement>(".cursor-follower")!
+
+    element.classList.add(".hidden")
   }
 
   ngOnInit(): void {
+    this.setX = GSAP.quickSetter(".cursor-follower", "left", "px")
+    this.setY = GSAP.quickSetter(".cursor-follower", "top", "px")
+
     this.animate()
   }
 
   public animate() {
     this.mouse.x.current = GSAP.utils.interpolate(this.mouse.x.current, this.mouse.x.target, this.mouse.x.ease)
     this.mouse.y.current = GSAP.utils.interpolate(this.mouse.y.current, this.mouse.y.target, this.mouse.y.ease)
-    console.log("🚀 ~ CursorInteractiveSlideComponent ~ animate ~ this.mouse.y.current :", this.mouse.y.current )
-    console.log("🚀 ~ CursorInteractiveSlideComponent ~ animate ~ this.mouse.x.current :", this.mouse.x.current )
-    
-    console.log()
+
     this.setX(this.mouse.x.current)
     this.setY(this.mouse.y.current)
-    
+
     this.animationId = requestAnimationFrame(() => this.animate())
   }
 
