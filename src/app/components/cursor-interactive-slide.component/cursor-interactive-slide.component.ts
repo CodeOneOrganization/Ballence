9c5c84d1905127eaad446e4a1c4df8527df7594c
douglas-front <douglasjknw007@gmail.com>
2024-11-26 18:41:17 -0300

    feat: making cursor interactive but not finished

--- src/app/components/cursor-interactive-slide.component/cursor-interactive-slide.component.ts
+++ src/app/components/cursor-interactive-slide.component/cursor-interactive-slide.component.ts
@@ -1,6 +1,6 @@
 import GSAP from "gsap"
 
-import { Component, HostListener, OnDestroy } from "@angular/core";
+import { AfterViewInit, Component, HostListener, Input, OnDestroy } from "@angular/core";
 
 @Component({
   templateUrl: "./cursor-interactive-slide.component.html",
@@ -10,7 +10,10 @@ import { Component, HostListener, OnDestroy } from "@angular/core";
   styleUrl: "./cursor-interactive-slide.component.scss",
 })
 
-export class CursorInteractiveSlideComponent implements OnDestroy {
+export class CursorInteractiveSlideComponent implements OnDestroy, AfterViewInit {
+
+  @Input() imgSrc!: string;
+
   private mouse = {
     x: {
       current: 0,
@@ -29,26 +32,26 @@ export class CursorInteractiveSlideComponent implements OnDestroy {
 
   @HostListener("document:mousemove", ["$event"])
   public onMouseMove(event: MouseEvent) {
-    this.mouse.x.target = event.clientX
-    this.mouse.y.target = event.clientY
-    
+    this.mouse.x.target = window.innerWidth - event.clientX ;  
+    this.mouse.y.target = window.innerHeight - event.clientY;
     console.log("onMouseMove fired")
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.animate()
+
+    this.setX = GSAP.quickSetter(".cursor-follower", "left", "px")
+    this.setY = GSAP.quickSetter(".cursor-follower", "top", "px")
   }
 
   public animate() {
     this.mouse.x.current = GSAP.utils.interpolate(this.mouse.x.current, this.mouse.x.target, this.mouse.x.ease)
     this.mouse.y.current = GSAP.utils.interpolate(this.mouse.y.current, this.mouse.y.target, this.mouse.y.ease)
-    console.log("🚀 ~ CursorInteractiveSlideComponent ~ animate ~ this.mouse.y.current :", this.mouse.y.current )
-    console.log("🚀 ~ CursorInteractiveSlideComponent ~ animate ~ this.mouse.x.current :", this.mouse.x.current )
     
-    console.log()
+ 
     this.setX(this.mouse.x.current)
     this.setY(this.mouse.y.current)
-    
+
     this.animationId = requestAnimationFrame(() => this.animate())
   }
 
