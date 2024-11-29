3742f2d295153d564d37ab7f26c2dd10a090ca61
HuannAnd <huannvicente14@outlook.com>
2024-11-29 16:04:46 -0300

    fix: wrong position of cursor follower resolved

--- src/app/layouts/about/about.component.ts
+++ src/app/layouts/about/about.component.ts
@@ -29,43 +29,19 @@ export class AboutComponent implements AfterViewInit {
     const imageSliderRef = document.querySelectorAll<HTMLImageElement>('.image-slider');
 
     imageSliderRef.forEach((imageElement) => {
-      // imageElement.addEventListener("")
-
-      imageElement.addEventListener("mousemove", () => {
-
-      })
 
       imageElement.addEventListener("mouseenter", (e) => {
         const imageSrc = String(imageElement.dataset["imageSrc"])
-        // const elementCenterPosition = e.offsetX + eoco
-        // console.log("e.clientWidth", imageElement.clientWidth)
-        // console.log("e.clientHeight", imageElement.clientHeight)
-        // console.log("e.offsetX", e.offsetX)
-        // console.log("e.offsetY", e.offsetY)
-        const imageLeft = window.innerWidth - imageElement.offsetLeft
-        console.log("🚀 ~ AboutComponent ~ imageElement.addEventListener ~ imageLeft:", imageLeft)
-        const x_ci = (imageElement.clientWidth / 2) + imageLeft
-        // console.log("🚀 ~ AboutComponent ~ imageElement.addEventListener ~ x_ci:", x_ci)
-        const y_ci = (imageElement.clientHeight / 2) + imageElement.scrollTop
-        // console.log("🚀 ~ AboutComponent ~ imageElement.addEventListener ~ y_ci:", y_ci)
-        const x_c = e.clientX
-        const y_c = e.clientY
-
+        const { x, y, width, height } = imageElement.getBoundingClientRect()
+        const x_ci = (width / 2) + x
+        const y_ci = (height / 2) + y
 
         this.isMouseEventMen = true;
         this.isMouseEvent = true
-        // console.log('Mouse Enter:', this.isMouseEventMen);
         this.image = imageSrc
-        // this.cursorService.setCursor()
 
-        this.cursorService.setCursor({
-          x_c,
-          y_c,
-          x_ci,
-          y_ci
-        })
+        this.cursorService.setCursor({ x_ci, y_ci })
       })
-        console.log("🚀 ~ AboutComponent ~ imageElement.addEventListener ~ imageLeft:", imageLeft)
 
       imageElement?.addEventListener('mouseleave', () => {
         this.isMouseEventMen = false;
