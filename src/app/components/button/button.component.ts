fa56f8832b8e7efea003f90d7a8a09df2a727cfb
douglas-front <douglasjknw007@gmail.com>
2025-01-18 13:05:32 -0300

    feat: animação de mouseEnter e mouseLeave no button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LenisScrollService } from '../../services/LenisScrollService.service';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-button',
@@ -9,8 +10,40 @@ import { LenisScrollService } from '../../services/LenisScrollService.service';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-  
+
+  @ViewChild('content1') content1Ref!: ElementRef
+  @ViewChild('content2') content2Ref!: ElementRef
+
   constructor(private lenisScrollService: LenisScrollService){}
+
+  onMouseEnter(): void{
+    gsap.to(this.content1Ref.nativeElement,{
+      y: "-100%",
+      duration: 0.5,
+      ease: "power2.inOut"
+    })
+
+    gsap.to(this.content2Ref.nativeElement,{
+      y: "-100%",
+       duration: 0.5,
+      ease: "power2.inOut"
+    })
+  }
+
+  onMouseLeave(){
+    gsap.to(this.content1Ref.nativeElement,{
+      y: "0%",
+      duration: 0.5,
+      ease: "power2.inOut"
+    })
+
+    gsap.to(this.content2Ref.nativeElement,{
+      y: "0%",
+       duration: 0.5,
+      ease: "power2.inOut"
+    })
+  }
+
 
   onClick(link: string | number): void{
     this.lenisScrollService.scrollTo(link)
